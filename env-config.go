d514392c5e9c4259deb584c3f41711f01a52b5b0
bpriyatmojo <budhi.priyatmojo@amartha.com>
2022-02-21 01:25:14 +0700

    update logger

--- env-config.go
+++ env-config.go
@@ -1,11 +1,5 @@
 package env_config
 
-import (
-	"context"
-
-	"go.uber.org/zap"
-)
-
 type Config interface {
 	GetInt(key string) int64
 	GetString(key string) string
@@ -14,14 +8,4 @@ type Config interface {
 	GetBinary(key string) []byte
 	GetArray(key string) []string
 	GetMap(key string) map[string]string
-}
-
-// Logger Interface. All methods SHOULD be safe for concurrent use.
-type Logger interface {
-	// Info logs a message at Info level
-	Info(ctx context.Context, msg string, fields ...zap.Field)
-	// Warn logs a message at Warn level
-	Warn(ctx context.Context, msg string, fields ...zap.Field)
-	// Error logs a message at Errors level
-	Error(ctx context.Context, msg string, err error)
 }
--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"context"
+
 	"github.com/blendle/zapdriver"
 	"go.uber.org/zap"
 )
@@ -14,4 +16,14 @@ func NewLogger() *zap.SugaredLogger {
 	sugar := logger.Sugar()
 
 	return sugar
+}
+
+// Logger Interface. All methods SHOULD be safe for concurrent use.
+type Logger interface {
+	// Info logs a message at Info level
+	Info(ctx context.Context, msg string, fields ...zap.Field)
+	// Warn logs a message at Warn level
+	Warn(ctx context.Context, msg string, fields ...zap.Field)
+	// Error logs a message at Errors level
+	Error(ctx context.Context, msg string, err error)
 }
